755458e48f58b8e6cd145d812823621697ac82e0
Leandro Gonçalves <leandrow01s@gmail.com>
2018-06-28 15:53:37 -0300

    Adds plugins: gulp-uglify, gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
-var gulp = require('gulp'),
-		stylus = require('gulp-stylus');
+let gulp     = require('gulp'),
+		stylus   = require('gulp-stylus'),
+		uglify   = require('gulp-uglify'),
+		cleanCSS = require('gulp-clean-css');
 
 gulp.task('default', function () {
 
 	// CSS
 	gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css').pipe(gulp.dest('public/lib/css'));
-	gulp.src('resources/assets/docs/prism.css').pipe(gulp.dest('public/lib/css'));
+	gulp.src('resources/assets/docs/prism.css')
+		.pipe(cleanCSS({compatibility: '*'}))
+		.pipe(gulp.dest('public/lib/css'));
 
 	// Stylus
 	gulp.src('resources/assets/stylus/base/docs.styl')
 			.pipe(stylus())
+			.pipe(cleanCSS({compatibility: '*'}))
 			.pipe(gulp.dest('public/lib/css'));
 
 	// Js
@@ -18,5 +23,8 @@ gulp.task('default', function () {
 	gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js').pipe(gulp.dest('public/lib/js'));
 	gulp.src('node_modules/clipboard/dist/clipboard.min.js').pipe(gulp.dest('public/lib/js'));
 	gulp.src('resources/assets/docs/prism.js').pipe(gulp.dest('public/lib/js'));
+	gulp.src('resources/assets/docs/docs.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('public/lib/js'));
 
 });
